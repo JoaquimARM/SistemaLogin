5d84b801cdaec2ca299f9cff4e28d40cd93a5f9c
JoaquimARM <joaquimarm13@gmail.com>
2024-09-17 11:43:18 -0300

    login

---
+++ src/main/java/com/sesi/login/config/configuracaoSeguranca.java
@@ -0,0 +1,20 @@
+package com.sesi.login.config;
+
+import java.beans.JavaBean;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class configuracaoSeguranca {
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity htpp) throws Exception{
+		htpp.authorizeHttpRequests((authorize) ->
+		authorize
+			.requestMatchers("/login","/registrar").permitAll()
+				);
+		return htpp.build();
+	}
+}
